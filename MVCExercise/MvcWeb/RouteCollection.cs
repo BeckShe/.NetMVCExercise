d76628e2c69886d1b8fd87e6e0f0b731b2dca48f
CHENJIE-PC\Administrator <530216775@qq.com>
2019-06-10 15:43:36 +0800

    mvc学习

---
+++ MVCExercise/MvcWeb/RouteCollection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace MvcWeb
+{
+    public class RouteCollection:Collection<RouteBase>
+    {
+        public RouteData GetRouteData(HttpContextBase httpContext)
+        {
+            foreach (RouteBase route in this)
+            {
+                var routeData = route.GetRouteData(httpContext);
+                if (null!=routeData)
+                {
+                    return routeData;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ MVCExercise/MvcWeb/RouteData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace MvcWeb
+{
+    public class RouteData
+    {
+        public string Controller { get; set; }
+
+        public string Action { get; set; }
+
+        public IList<string> Assemblies { get; set; }
+
+        public IRouteHandler RouteHandler { get; set; }
+
+        public RouteData(string controller, string action, IRouteHandler routeHandler)
+        {
+            this.Controller = controller;
+            this.Action = action;
+            this.RouteHandler = routeHandler;
+        }
+    }
+}
