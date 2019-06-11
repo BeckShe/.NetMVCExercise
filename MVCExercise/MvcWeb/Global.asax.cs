3361f9be69a8c153f171c226402b0ad4795dde6c
CHENJIE-PC\Administrator <530216775@qq.com>
2019-06-11 14:17:23 +0800

    MVC

---
+++ MVCExercise/MvcWeb/Global.asax.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace MvcWeb
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteTable.Routes.Add(new QueryStringRoute());
+            RouteTable.Assemblies.Add("MvcWeb, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null");
+            RouteTable.Namespaces.Add("Artech.MvcWeb");
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MVCExercise/MvcWeb/QueryStringRoute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace MvcWeb
+{
+    public class QueryStringRoute:RouteBase
+    {
+        public override RouteData GetRouteData(HttpContextBase httpContext)
+        {
+            if (httpContext.Request.QueryString.AllKeys.Contains("controller"))
+            {
+                string controller = httpContext.Request.QueryString["controller"];
+                string action = httpContext.Request.QueryString["action"];
+                IRouteHandler routeHandler = new MvcRouteHandler();
+                return new RouteData(controller,action,routeHandler);
+            }
+            return null;
+        }
+    }
+
+    public class MvcRouteHandler : IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return new MvcHandler(requestContext);
+        }
+    }
+}
